67dd354293b21f69b903be34d23332f1665574e0
RinzlerN26 <Email>
2022-11-15 23:31:22 +0530

    Commit by NR 3

--- app/src/main/java/com/buzz/club/Signupactivity.kt
+++ app/src/main/java/com/buzz/club/Signupactivity.kt
@@ -7,6 +7,8 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_signinactivity.*
 import kotlinx.android.synthetic.main.activity_signupactivity.*
 
@@ -39,10 +41,10 @@ class Signupactivity : AppCompatActivity() {
         val email=email_signup.text.toString()
         val password=password_signup.text.toString()
         when{
-            TextUtils.isEmpty(fullName)->Toast.makeText(this,"full name is required",Toast.LENGTH_LONG)
-            TextUtils.isEmpty(userName)->Toast.makeText(this,"user name is required",Toast.LENGTH_LONG)
-            TextUtils.isEmpty(email)->Toast.makeText(this,"email is required",Toast.LENGTH_LONG)
-            TextUtils.isEmpty(password)->Toast.makeText(this,"password  is required",Toast.LENGTH_LONG)
+            TextUtils.isEmpty(fullName)->Toast.makeText(this,"full name is required",Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(userName)->Toast.makeText(this,"user name is required",Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(email)->Toast.makeText(this,"email is required",Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(password)->Toast.makeText(this,"password  is required",Toast.LENGTH_LONG).show()
 
             else->{
 
@@ -55,11 +57,11 @@ class Signupactivity : AppCompatActivity() {
                 mAuth.createUserWithEmailAndPassword(email, password).
                         addOnCompleteListener{task->
                     if(task.isSuccessful){
-                       saveUserInfo(fullName,userName,email)
+                       saveUserInfo(fullName,userName,email,progressDialog)
                     }
                     else{
                         val message=task.exception!!.toString()
-                        Toast.makeText(this,"error:$message",Toast.LENGTH_LONG)
+                        Toast.makeText(this,"error:$message",Toast.LENGTH_LONG).show()
                         mAuth.signOut()
                         progressDialog.dismiss()
                     }
@@ -72,7 +74,35 @@ class Signupactivity : AppCompatActivity() {
 
     }
 
-    private fun saveUserInfo(fullName: String, userName: String, email: String) {
+    private fun saveUserInfo(fullName: String, userName: String, email: String,progressDialog:ProgressDialog) {
+               val currentUserID=FirebaseAuth.getInstance().currentUser!!.uid
+               val usersRef:DatabaseReference=FirebaseDatabase.getInstance().reference.child("Users")
+               val userMap=HashMap<String,Any>()
+               userMap["uid"]=currentUserID
+               userMap["fullname"]=currentUserID
+               userMap["username"]=currentUserID
+               userMap["email"]=currentUserID
+               userMap["bio"]="hey i am using buzzclub"
+userMap["image"]="https://firebasestorage.googleapis.com/v0/b/buzzclub-7272b.appspot.com/o/Default%20Images%2Fprofile.png?alt=media&token=485c0f1f-3fb2-4c96-b7b7-7fd991302531"
+    usersRef.child(currentUserID).setValue(userMap)
+        .addOnCompleteListener{
+            task->
+            if(task.isSuccessful){
+                progressDialog.dismiss()
+                Toast.makeText(this,"account has been created successfully",Toast.LENGTH_LONG).show()
+                val intent=Intent(this@Signupactivity,MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
+            }
+            else{
+                val message=task.exception!!.toString()
+                Toast.makeText(this,"error:$message",Toast.LENGTH_LONG).show()
+                FirebaseAuth.getInstance().signOut()
+                progressDialog.dismiss()
+
+            }
+        }
 
     }
 }
--- app/src/main/java/com/buzz/club/signinactivity.kt
+++ app/src/main/java/com/buzz/club/signinactivity.kt
@@ -3,6 +3,7 @@ package com.buzz.club
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_signinactivity.*
 
 class signinactivity : AppCompatActivity() {
@@ -13,4 +14,15 @@ class signinactivity : AppCompatActivity() {
             startActivity(Intent(this, Signupactivity::class.java))
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        if(FirebaseAuth.getInstance().currentUser!=null){
+            val intent=Intent(this@signinactivity,MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+        }
+    }
+
 }
